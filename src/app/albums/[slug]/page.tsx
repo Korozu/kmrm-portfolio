1deafb466d1104954f06c4df321af2ce02ec39b4
RemiDelaval <remi.delaval.partner@decathlon.com>
2026-03-02 16:56:34 +0100

    feat: Add generateStaticParams function to support dynamic album slugs

--- src/app/albums/[slug]/page.tsx
+++ src/app/albums/[slug]/page.tsx
@@ -2,6 +2,12 @@ import {allAlbums} from "contentlayer/generated";
 import {notFound} from "next/navigation";
 import AlbumGallery from "@/components/AlbumGallery";
 
+export async function generateStaticParams() {
+    return allAlbums.map((album) => ({
+        slug: album.slug,
+    }));
+}
+
 interface PageParams {
     params: { slug: string };
 }
